6efd008028d442cf4149c76b75b556119aaba8d9
Patrick Bald <pbaldvt@gmail.com>
2024-06-14 15:22:56 -0700

    Initialize

---
+++ clean.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def clean_data():
+    pass
+
+def ingest_data():
+    pass
+
+def main():
+    print('Cleaning Snowplow Dataset . . .')
+    
+
+    pass
+
+if __name__ == '__main__':
+    main()
