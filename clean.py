0ee1b83b3a706e4f50705af5667d57629bf14842
Patrick Bald <pbaldvt@gmail.com>
2024-06-14 21:39:40 -0700

    filling empty cols, fixing datetime, rename analsis file

--- clean.py
+++ clean.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
 import pandas as pd
+import uuid
 
 def clean_data(df):
 
-    # Remove null values
-    df.dropna(inplace=True)
+    # Remove null values from essential columns
+    non_null_cols = ['event_id', 'page_view_id', 'session_id', 'page_url', 'page_urlhostname', 'dvce_created_tstamp', 'referral_url']
+    df.dropna(subset=non_null_cols, inplace=True)
+
+    # Fill Geo Cols
+    df = df.fillna({'geo_country': 'Unknown', 'geo_region': 'Unknown', 'geo_city': 'Unknown', 'geo_timezone': 'Unknown'})
 
     # Drop Duplicates
     df.drop_duplicates(inplace=True)
@@ -13,7 +18,11 @@ def clean_data(df):
     # Fix Datetime columns
     df['dvce_created_tstamp'] = pd.to_datetime(df['dvce_created_tstamp'], errors='coerce')
 
+    # Strip session ids
+    df['session_id'] = df['session_id'].str.strip()
+
     # Fix Urls
+
 
     return df
 
@@ -25,10 +34,12 @@ def write_file(df, path):
     df.to_csv(path, index=False)
 
 def main():
+    # File Names
+    cleaned_data_name = 'cleaned-dataset-' + str(uuid.uuid4())
 
     # File Paths
     dataset_path = '../snowplow-dataset.csv'
-    cleaned_data_path = '../snowplow_dataset_cleaned.csv'
+    cleaned_data_path = '../cleaned-datasets/' + cleaned_data_name
 
     print('Cleaning dataset . . .')
     
